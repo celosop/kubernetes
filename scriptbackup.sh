175f1574a1e56a0b819efe75030c0c39f5d623b4
Marcelo Silva <celosop@gmail.com>
2020-11-05 22:44:24 -0300

    script backup sites

---
+++ scriptbackup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Mudando o diretorio para a pasta de backup
+cd /home/celosop/Documents
+
+# Calculando a hora e o dia do backup
+ident=`date +%Y%m%d%H%M`
+
+while
+
+#!/bin/bash
+
+ident=`date +%Y%m%d%H%M`
+
+cd /home/celosop/Documents/
+
+for item in *
+do
+        if [ -d "$item" ]
+        then
+          echo "a pasta $item foi compactada em TAR GZ"
+          tar -czvf $item$ident.tar.gz $item
+        fi
+done
+
+# Deixando o backup FULL desabilitado
+# Realizando o backup FULL para todos os databases
+# mysqldump --password=TKFpoi,@3 --all-databases -c --result-file=/backup/mysql-backup-FULL-$ident.sql > /dev/null
+
+# Realizando a compressao do backup no formato SQL para o TAR GZ
+# tar -czvf mysql-backup-FULL-$ident.sql.tar.gz mysql-backup-FULL-$ident.sql
+
+
+
+
+
+# Obtendo os nomes dos databases instalados no MYSQL
+# Os nomes sao gravados em um arquivo temporario "databases.txt"
+mysql -u root --password=TKFpoi,@3 -e 'show databases;' > /tmp/databases-names.txt
+
+# Realizando o backup dos databases individualmente
+while read LINE # Lendo cada linha (database) do arquivo com os nomes
+do
+        if [ $LINE != 'Database' ] && [ $LINE != 'sys' ] && [ $LINE != 'information_schema' ] && [ $LINE != 'performance_schema' ]; then
+                mysqldump --password=TKFpoi,@3 --databases $LINE -c --result-file=/backup/mysql-backup-$LINE-$ident.sql > /dev/null
+                tar -czvf mysql-backup-$LINE-$ident.sql.tar.gz mysql-backup-$LINE-$ident.sql
+                rm -rf *.sql
+        fi
+done < /tmp/databases-names.txt
+
+# Removendo os arquivos com extensao SQL
+rm -rf *.sql
+
+# Removendo os backups mais antigos (+20 dias)
+# cd /backup
+# find -atime +20 -exec rm -vrf {} \;
